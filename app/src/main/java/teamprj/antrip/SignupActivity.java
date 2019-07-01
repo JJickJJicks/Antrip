268da94b6fb75987030f05136277154fb4bddaeb
이재환 <wng2641@naver.com>
2019-07-01 16:59:44 +0900

    회원가입 DB 연결 (코드 최적화 X)

--- app/src/main/java/teamprj/antrip/SignupActivity.java
+++ app/src/main/java/teamprj/antrip/SignupActivity.java
@@ -4,17 +4,34 @@ package teamprj.antrip;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class SignupActivity extends Activity {
+
+
+    private static final String TAG = "signUp";
+    private static final String URL_FOR_REGISTRATION = "http://antrip.kro.kr/app/signup.php";
+    private EditText emailText, passwordText, pwCheckText, nameText, birthText;
 
     Calendar myCalendar = Calendar.getInstance();
     DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
@@ -26,7 +43,6 @@ public class SignupActivity extends Activity {
             updateLabel();
         }
     };
-    private EditText email, password, pwcheck, name, birth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +50,13 @@ public class SignupActivity extends Activity {
         //타이틀바 없애기
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_signup);
-        email = findViewById(R.id.emailText);
-        password = findViewById(R.id.pwText);
-        pwcheck = findViewById(R.id.pwcheckText);
-        name = findViewById(R.id.nameText);
-        birth = findViewById(R.id.birthText);
-        birth.setOnClickListener(new View.OnClickListener() {
+
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.pwText);
+        pwCheckText = findViewById(R.id.pwCheckText);
+        nameText = findViewById(R.id.nameText);
+        birthText = findViewById(R.id.birthText);
+        birthText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new DatePickerDialog(SignupActivity.this, myDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
@@ -47,10 +64,88 @@ public class SignupActivity extends Activity {
         });
     }
 
-    //확인 버튼 클릭
-    public void mOnClose(View v) {
-        //액티비티(팝업) 닫기
-        finish();
+    public void signUp(View v) {
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.pwText);
+        pwCheckText = findViewById(R.id.pwCheckText);
+        nameText = findViewById(R.id.nameText);
+        birthText = findViewById(R.id.birthText);
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+        String pwCheck = pwCheckText.getText().toString();
+        String name = nameText.getText().toString();
+        String birth = birthText.getText().toString();
+
+        if (checkError(email, password, pwCheck, name, birth)) {
+            register(email, password, name, birth);
+//            finish();
+        }
+    }
+
+    public boolean checkError(String email, String password, String pwCheck, String name, String birth) {
+        if (email.equals("") || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())
+            Toast.makeText(getApplicationContext(), R.string.wrongEmail, Toast.LENGTH_SHORT).show();
+        else if (password.equals("") || !password.equals(pwCheck))
+            Toast.makeText(getApplicationContext(), R.string.wrongPassword, Toast.LENGTH_SHORT).show();
+        else if (name.equals(""))
+            Toast.makeText(getApplicationContext(), R.string.wrongName, Toast.LENGTH_SHORT).show();
+        else if (birth.equals(""))
+            Toast.makeText(getApplicationContext(), R.string.wrongBirth, Toast.LENGTH_SHORT).show();
+        else
+            return true;
+        return false;
+    }
+
+    private void register(final String email, final String password, final String name, final String birth) {
+        // Tag used to cancel the request
+        String cancel_req_tag = "register";
+
+        StringRequest strReq = new StringRequest(Request.Method.POST, URL_FOR_REGISTRATION, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response);
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    if (!error) {
+                        String user = jObj.getJSONObject("user").getString("name");
+                        Toast.makeText(getApplicationContext(), R.string.welcome + user, Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Registration Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                final String types = "1";
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("password", password);
+                params.put("name", name);
+                params.put("birth", birth);
+                params.put("type", types);
+                return params;
+            }
+        };
+        // Adding request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq, cancel_req_tag);
     }
 
     @Override
@@ -62,7 +157,7 @@ public class SignupActivity extends Activity {
     private void updateLabel() {
         String myFormat = "yyyy-MM-dd";    // 출력형식   2019-06-26
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
-        birth.setText(sdf.format(myCalendar.getTime()));
+        birthText.setText(sdf.format(myCalendar.getTime()));
     }
 
 }
