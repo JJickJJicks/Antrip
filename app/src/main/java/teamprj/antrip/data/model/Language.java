45f3bfcc327a0453e6482d8cc0ac293f55b8648e
이재환 <wng2641@naver.com>
2019-10-01 19:11:26 +0900

    미지원 선택 언어 제거, 시작 화면 스플래시로 롤백

--- app/src/main/java/teamprj/antrip/data/model/Language.java
+++ app/src/main/java/teamprj/antrip/data/model/Language.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Language {
     HashMap<String, String> lang_code = new HashMap<>();
-    String[][] lang_data = {{"한국어", "ko"}, {"일본어", "ja"}, {"중국어 간체", "zh-cn"}, {"중국어 번체", "zh-tw"}, {"힌디어", "hi"}, {"영어", "en"}, {"스페인어", "es"}, {"프랑스어", "fr"}, {"독일어", "de"}, {"포르투갈어", "pt"}, {"베트남어", "vi"}, {"인도네시아어", "id"}, {"페르시아어", "fa"}, {"아랍어", "ar"}, {"미얀마어", "mm"}, {"태국어", "th"}, {"러시아어", "ru"}, {"이탈리아어", "it"}, {"알수없음", "unk"}, {"자동 감지", "user"}};
+    String[][] lang_data = {{"한국어", "ko"}, {"일본어", "ja"}, {"중국어 간체", "zh-cn"}, {"영어", "en"}, {"자동 감지", "user"}};
 
     public Language() {
         for (String[] i : lang_data)
