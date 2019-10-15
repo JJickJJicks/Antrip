71bf8f00b98f1064c9e7cf65f55388b7e06f9b4c
이태구 <rksk1116@naver.com>
2019-10-15 23:20:45 +0900

    숙소 하나만 추가되도록 변경, 임시 Json 변경

--- app/src/main/java/teamprj/antrip/ui/function/InputPlanActivity.java
+++ app/src/main/java/teamprj/antrip/ui/function/InputPlanActivity.java
@@ -12,6 +12,7 @@ import android.widget.CheckBox;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AddressComponent;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
@@ -21,6 +22,7 @@ import java.util.Arrays;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 import teamprj.antrip.R;
+import teamprj.antrip.ui.MainActivity;
 
 
 public class InputPlanActivity extends FragmentActivity {
@@ -46,7 +48,7 @@ public class InputPlanActivity extends FragmentActivity {
         }
         AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
                 getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG));
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.ADDRESS, Place.Field.ADDRESS_COMPONENTS));
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(@NonNull Place selectPlace) {
@@ -75,8 +77,21 @@ public class InputPlanActivity extends FragmentActivity {
                                 }
                             });
                     builder.show();
+                } else if (accommodationCheck.isChecked() && TravelPlanActivity.checkAccommodation(index) == -1) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(InputPlanActivity.this);
+                    builder.setTitle("숙소를 이미 선택하셨습니다.");
+                    builder.setMessage("숙소는 하루에 한 곳만 지정할 수 있습니다.");
+                    builder.setPositiveButton("확인",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            });
+                    builder.show();
                 } else {
                     finish();
+//                    Log.d("add", place.getAddress());
+//                    String country = place.getAddressComponents().asList().get(place.getAddressComponents().asList().size() - 1).getName();
+//                    Log.d("country", country);
                     TravelPlanActivity.addItem(index, place.getName(), latLng, accommodationCheck.isChecked());
                 }
             }
